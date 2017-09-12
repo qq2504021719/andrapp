bd9d192c5f6b10541b148d6a1a006faaa727cb7c
qq2504021719 <1104534940@qq.com>
2017-09-12 21:29:49 +0800

    安卓6以上，需要单独获取定位权限

--- XunDian/app/src/main/java/com/bignerdranch/android/xundian/xundianguanli/XunDianGuanLiActivity.java
+++ XunDian/app/src/main/java/com/bignerdranch/android/xundian/xundianguanli/XunDianGuanLiActivity.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.xundian.xundianguanli;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -52,7 +53,8 @@ import java.util.List;
 public class XunDianGuanLiActivity extends NeiYeCommActivity {
 
     private static final String EXTRA = "com.bignerdranch.android.xundian.xundianguanli.XunDianGuanLiActivity";
-
+    private final int SDK_PERMISSION_REQUEST = 127;
+    private String permissionInfo;
 
     private Context mContext;
 
@@ -286,6 +288,8 @@ public class XunDianGuanLiActivity extends NeiYeCommActivity {
      * 百度地图定位调用
      */
     public void BaiDuDingWeiDiaoYong(){
+        // after andrioid m,must request Permiision on runtime
+        getPersimmions();
         mCurrentMode = LocationMode.NORMAL;
         mMapView = (MapView) findViewById(R.id.bmapView); //找到我们的地图控件
         mBaiduMap = mMapView.getMap(); //获得地图
@@ -304,7 +308,58 @@ public class XunDianGuanLiActivity extends NeiYeCommActivity {
         mLocClient.setLocOption(option);
         mLocClient.start();
     }
+    @TargetApi(23)
+    private void getPersimmions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissions = new ArrayList<String>();
+            /***
+             * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
+             */
+            // 定位精确位置
+            if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+			/*
+			 * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
+			 */
+            // 读写权限
+            if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
+            }
+            // 读取电话状态权限
+            if (addPermission(permissions, Manifest.permission.READ_PHONE_STATE)) {
+                permissionInfo += "Manifest.permission.READ_PHONE_STATE Deny \n";
+            }
 
+            if (permissions.size() > 0) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            }
+        }
+    }
+    @TargetApi(23)
+    private boolean addPermission(ArrayList<String> permissionsList, String permission) {
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) { // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
+            if (shouldShowRequestPermissionRationale(permission)){
+                return true;
+            }else{
+                permissionsList.add(permission);
+                return false;
+            }
+
+        }else{
+            return true;
+        }
+    }
+    @TargetApi(23)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // TODO Auto-generated method stub
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
     /**
      * 百度定位
      */
